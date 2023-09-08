c1d5682865882009d693e012e21b2502c74d6d8b
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-09-09 04:53:40 +0800

    71

---
+++ 71simlifyPath/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+    string simplifyPath(string path) {
+
+    }
+};
+int main(int argc, char** argv)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
