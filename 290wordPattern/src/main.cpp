c6703f64bd12606f1a197756022f734f64cfba66
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-09-08 12:07:27 +0800

    290

---
+++ 290wordPattern/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+
+    }
+};
+
+int main(int argc, char** argv)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
