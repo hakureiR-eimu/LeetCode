176107593f5228b97da598e2ef01dc8d4d757fdb
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-09-10 21:03:34 +0800

    71f

--- 71simlifyPath/src/main.cpp
+++ 71simlifyPath/src/main.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    string simplifyPath(string path) {
+    static string simplifyPath(string path) {
+        vector<string> stk;
+        string res = "/";
+        int cnt;
+        int count;
+        for(int i = 0; i < path.length();){
+            if(path[i] == '/'){
+                string tmp = "";
+                i++;
+                continue;
+            }
+            else {
+                string tmp = "";
+                cnt = 0;
+                count = 0;
+                while(path[i]!= '/' && i < path.length()){
+                    count ++;
+                    if(path[i] == '.') {
+                        cnt++;
+                        //cout << cnt << endl;
+                    }
+                    tmp.push_back(path[i]);
+                    i++;
+                }
+                if(cnt == 2 && !stk.empty() && count == 2){
+                    //cout << "suc" << endl;
+                    stk.pop_back();
+                    continue;
+                }
+                else if(cnt == 2 && stk.empty() && count == 2){
+                    continue;
+                }
+                else if(cnt == 1 && count == 1){
+                    tmp = "";
+                    cnt = 0;
+
+                }
+                else{
+                    //cout << tmp <<endl;
+                    stk.push_back(tmp);
+                }
+
+            }
+        }
+        if(stk.empty()){
+            return "/";
+        }
+        for(string& ch:stk){
+            res += ch;
+            if(!(&ch == &stk.back())){
+                res += '/';
+            }
+        }
+        return res;
 
     }
 };
 int main(int argc, char** argv)
 {
-    cout << "hello world!" << endl;
+    //cout << "hello world!" << endl;
+    string res = Solution::simplifyPath("/.hidden");
+    cout << res <<endl;
     return 0;
 }
