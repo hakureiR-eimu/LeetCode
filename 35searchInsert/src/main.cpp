512c8227c826ed1ff98813f89f52fb5b62b8cfbb
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-09-17 15:44:13 +0800

    35f

---
+++ 35searchInsert/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int searchInsert(vector<int> &nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    int mid = left + (right - left) / 2;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (target > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return left;
+  }
+};
+
+int main(int argc, char **argv) {
+  cout << "hello world!" << endl;
+  return 0;
+}
