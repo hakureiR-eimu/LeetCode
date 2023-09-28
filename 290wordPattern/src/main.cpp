51bba769c37151ce0ac7aa67b1955fb7daf622dc
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-09-28 09:28:49 +0800

    290f

--- 290wordPattern/src/main.cpp
+++ 290wordPattern/src/main.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool wordPattern(string pattern, string s) {
-        unordered_map<char, string> patternToS;
-        unordered_map<string, char> sToPattern;
-        if(pattern.length() != s.length()){
+    bool wordPattern(string pattern, string s)
+    {
+        unordered_map<char, string> patternToString;
+        unordered_map<string, char> stringToPattern;
+        vector<string> arrS;
+        string tmp = "";
+        s += ' ';
+        for (char ch : s)
+        {
+            if (ch == ' ')
+            {
+                arrS.push_back(tmp);
+                tmp = "";
+            }
+            else
+            {
+                tmp += ch;
+            }
+        }
+
+        // cout << arrS.size() << endl;
+        // for (string tmp : arrS)
+        // {
+        //     cout << tmp << endl;
+        // }
+        if (pattern.length() != arrS.size())
+        {
             return false;
         }
-        
-
-
+        int n = arrS.size();
+        for (int i = 0; i < n; ++i)
+        {
+            if (patternToString.count(pattern[i]) &&
+                patternToString[pattern[i]] != arrS[i])
+                return false;
+            if (stringToPattern.count(arrS[i]) &&
+                stringToPattern[arrS[i]] != pattern[i])
+                return false;
+            patternToString[pattern[i]] = arrS[i];
+            stringToPattern[arrS[i]] = pattern[i];
+        }
+        return true;
     }
 };
 
 int main(int argc, char** argv)
 {
-    cout << "hello world!" << endl;
+    string pattern = "abba", s = "dog cat cat dog";
+    Solution* p = new Solution();
+    bool res = p->wordPattern(pattern, s);
+    cout << res << endl;
     return 0;
 }
