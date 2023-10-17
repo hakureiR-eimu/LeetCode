ce947ba44adc0793c6d1b926b58f3ab56200a6a9
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-10-17 10:50:32 +0800

    2361f

---
+++ 236lowestCommonAncestor/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode* ans;
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) ||
+            (root->val == p->val || root->val == q->val) && (lson || rson))
+        {
+            ans = root;
+        }
+        return lson || rson || (root->val == p->val || root->val == q->val);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        dfs(root, p, q);
+        return ans;
+    }
+};
+int main(int argc, char** argv)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
