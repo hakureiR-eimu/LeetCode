db9e44202925e4823fc4beda3f6078037a590622
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-10-23 21:02:26 +0800

    173f

--- 173BSTIterator/src/main.cpp
+++ 173BSTIterator/src/main.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
+#include <queue>
 #include <stack>
-
 using namespace std;
 
-struct TreeNode
-{
-    int val;
+struct TreeNode {
+    int       val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right)
-    {
-    }
+    TreeNode()
+        : val( 0 )
+        , left( nullptr )
+        , right( nullptr ) {}
+    TreeNode( int x )
+        : val( x )
+        , left( nullptr )
+        , right( nullptr ) {}
+    TreeNode( int x, TreeNode *left, TreeNode *right )
+        : val( x )
+        , left( left )
+        , right( right ) {}
 };
 
-class BSTIterator
-{
-private:
-    stack<TreeNode *> stk;
-    void midOrder(TreeNode *root)
-    {
-        if (root->left)
-        {
-            midOrder(root->left);
+class BSTIterator {
+  private:
+    queue<TreeNode *> stk;
+    void              midOrder( TreeNode *root ) {
+                     if ( root->left ) {
+                         midOrder( root->left );
         }
-        stk.push(root);
-        if (root->right)
-        {
-            midOrder(root->right);
+                     stk.push( root );
+                     if ( root->right ) {
+                         midOrder( root->right );
         }
     }
 
-public:
-    BSTIterator(TreeNode *root) { midOrder(root); }
+  public:
+    BSTIterator( TreeNode *root ) { midOrder( root ); }
 
-    int next() {}
+    int next() {
+        int n = stk.front()->val;
+        stk.pop();
+        return n;
+    }
 
-    bool hasNext() {}
+    bool hasNext() { return !stk.empty(); }
 };
 
 /**
@@ -47,8 +52,7 @@ public:
  * int param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
-int main(int argc, char **argv)
-{
+int main( int argc, char **argv ) {
     cout << "hello world!" << endl;
     return 0;
 }
