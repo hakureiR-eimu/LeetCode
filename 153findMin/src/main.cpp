11c6e2bc6d42392bef669109c402f917730ef9c1
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-11-05 19:35:24 +0800

    153f

---
+++ 153findMin/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int findMin( vector<int> &nums ) {
+        int left = 0, right = nums.size() - 1;
+        while ( left < right ) {
+            int mid = left + ( right - left ) / 2;
+            if ( nums[ mid ] < nums[ right ] ) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[ left ];
+    }
+};
+
+int main( int argc, char **argv ) {
+    cout << "hello world!" << endl;
+    return 0;
+}
