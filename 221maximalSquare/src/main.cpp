c20e15d31ecf0a9e6a62ee634e4adc00c3049586
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-11-21 11:31:01 +0800

    221init

---
+++ 221maximalSquare/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+
+    }
+};
+
+int main(int argc, char** argv)
+{
+    vector<vector<char>> matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+    cout << "hello world!" << endl;
+    return 0;
+}
