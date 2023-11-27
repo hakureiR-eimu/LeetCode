93c000cbf6a2b886b01c07e993a586c563ad123b
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-11-27 22:39:23 +0800

    feat:31f

--- 3lengthOfLongestSubstring/src/main.cpp
+++ 3lengthOfLongestSubstring/src/main.cpp
@@ -1,16 +1,32 @@
+#include <deque>
 #include <iostream>
-
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
-
 class Solution {
   public:
     int lengthOfLongestSubstring( string s ) {
-        int letters[ 26 ] = { 0 };
-        int left = 0, right = 0;
-        int *p;
+        if ( s.size() == 0 ) return 0;
+        unordered_set<char> lookup;
+        int maxStr = 0;
+        int left = 0;
+        for ( int i = 0; i < s.size(); i++ ) {
+            while ( lookup.find( s[ i ] ) != lookup.end() ) {
+                lookup.erase( s[ left ] );
+                left++;
+            }
+            maxStr = max( maxStr, i - left + 1 );
+            lookup.insert( s[ i ] );
+        }
+        return maxStr;
     }
 };
+
 int main( int argc, char **argv ) {
-    cout << "hello world!" << endl;
+    string s = "tmmzuxt";
+    Solution *p = new Solution;
+    int len = p->lengthOfLongestSubstring( s );
+    cout << len << endl;
     return 0;
 }
