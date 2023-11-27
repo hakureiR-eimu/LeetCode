25bf499a1dd121c6ec4485bd188f7eecd47118ea
CodeOfUsamiWSL <zhang0285@foxmail.com>
2023-11-27 22:09:07 +0800

    feat:.gitignore renew

--- 3lengthOfLongestSubstring/src/main.cpp
+++ 3lengthOfLongestSubstring/src/main.cpp
@@ -1,38 +1,16 @@
-#include <cstdint>
 #include <iostream>
 
 using namespace std;
 
-class Solution
-{
-public:
-    int lengthOfLongestSubstring(string s)
-    {
-        int l = 0, r = 0, max = INT32_MIN;
-        int length;
-        int letters[26] = {0};
-        if (s.length() == 0) return 0;
-        for (int i = 0; i < s.length(); ++i)
-        {
-            if (letters[s[i]] == 0)
-            {
-                letters[s[i]]++;
-                r = i;
-                length = r - l + 1;
-                if (length > max) max = length;
-            }
-            else if (letters[s[i]] == 1)
-            {
-                letters[s[i]] = 0;
-                r = i;
-                l++;
-                length
-            }
-        }
+class Solution {
+  public:
+    int lengthOfLongestSubstring( string s ) {
+        int letters[ 26 ] = { 0 };
+        int left = 0, right = 0;
+        int *p;
     }
 };
-int main(int argc, char** argv)
-{
+int main( int argc, char **argv ) {
     cout << "hello world!" << endl;
     return 0;
 }
