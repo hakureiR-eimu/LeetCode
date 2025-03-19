a1a697fde633c56e56df1c0def88913826c07b3b
CodeOfUsami <zhang0285@foxmail.com>
2025-03-19 14:18:31 +0800

    feat:add 344

---
+++ 344reverseString/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+}
+
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
